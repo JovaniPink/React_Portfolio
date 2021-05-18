80d5f89035569117906179a799b1cd1c0798ee81
Jovani Pink <jovanirosado@hotmail.com>
2021-05-18 10:05:44 -0400

    fixed ESLINT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+// Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
