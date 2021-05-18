71bb6dda51930c045b94b70a2c656904985647e0
Jovani Pink <jovanirosado@hotmail.com>
2021-05-18 10:24:35 -0400

    Fixed the React formatting and copy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Home from "./components/pages/Home";
-import Portfolio2 from "./components/pages/Portfolio2";
-import Contact from "./components/pages/Contact";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './components/pages/Home';
+import Portfolio2 from './components/pages/Portfolio2';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Image from 'react-bootstrap/Image'
-import "./style.css"
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Image from 'react-bootstrap/Image';
+import './style.css';
 
 function Contact() {
-    return (
-        <Jumbotron id="jumbo">
-            <Image id="image" src="https://images.unsplash.com/photo-1504624244670-373b7257e140?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW91bnQlMjBwcmluY2V0b24lMjBjb2xvcmFkb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" ></Image>
-            <h1 id="header">Lets Connect!</h1>
-            <div id="contact">
-                <p id="info">
-                    <i id="contact" className="fas fa-mobile-alt"></i>
-                    812.320.4538</p>
-                <p id="info">
-                    <i id="contact" className="far fa-envelope"> </i> amoseman18@gmail.com </p>
-            </div>
-        </Jumbotron>
-    )
+  return (
+    <Jumbotron id="jumbo">
+      <Image
+        id="image"
+        src="https://images.unsplash.com/photo-1504624244670-373b7257e140?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW91bnQlMjBwcmluY2V0b24lMjBjb2xvcmFkb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+      />
+      <h1 id="header">Lets Connect!</h1>
+      <div id="contact">
+        <p id="info">
+          <i id="contact" className="fas fa-mobile-alt" />
+          812.320.4538
+        </p>
+        <p id="info">
+          <i id="contact" className="far fa-envelope">
+            {' '}
+          </i>{' '}
+          amoseman18@gmail.com{' '}
+        </p>
+      </div>
+    </Jumbotron>
+  );
 }
 
-export default Contact
+export default Contact;
